2de8dc6fa53655e319d4b49de217b0bede46012b
ShengyinWu <shengyinwu@ShengyinWus-Mac-mini.local>
2013-07-28 18:09:35 +0800

    clean mybow.py

--- mybow.py
+++ mybow.py
@@ -17,33 +17,34 @@ class BagOfVisualWords:
     def do_bag_of_visual_words(self):
         if not os.path.exists(self.bow_path):
             os.makedirs(self.bow_path)
-        files = os.listdir(self.desc_path)
-        n_sample = len(files)
-        wordstr = os.path.join("data", "words", "{0}.npy".format(word))
-        fd = file(wordstr, "rb")
-        words = np.load(fd)
+        files_in_dir = os.listdir(self.desc_path)
+        word_file = os.path.join(TOP_DIR, "data", "words", "{0}.npy".format(word))
+        fd = file(word_file, "rb")
+        codebook = np.load(fd)
         fd.close()
-        i = 0
-        for f in files:
+        for f in files_in_dir:
             print f
             if f[-1] == "y":
-                npyname = os.path.join(self.desc_path, f)
-                fd = file(npyname)
-                points = np.load(fd)
+                npy_file = os.path.join(self.desc_path, f)
+                # may be some files broken
+                if os.path.getsize(npy_file) < 300:
+                    continue
+                fd = file(npy_file)
+                keypoints = np.load(fd)
                 data = np.load(fd)
-                pngname = os.path.join(self.png_path, f[0:-4] + ".png")
-                im = Image.open(pngname)
-                WIDTH = im.size[0]
-                HEIGHT = im.size[1]
-                xs = points[:, 0]
-                ys = points[:, 1]
+                png_file = os.path.join(self.png_path, f[0:-4] + ".png")
+                loaded_image = Image.open(png_file)
+                image_width = loaded_image.size[0]
+                image_height = loaded_image.size[1]
+                xs = keypoints[:, 0]
+                ys = keypoints[:, 1]
 
-                n = np.sum(np.abs(data)**2, axis=-1)**(1./2) + 1e-12
-                nn = np.tile(n, (data.shape[1], 1))
-                data = data / nn.T
-                fd_fea = file(os.path.join(self.bow_path, f), "wb")
-                fea = vlad_coding(data, words, WIDTH, HEIGHT, xs, ys)
-                np.save(fd_fea, fea)
+                data_norm = np.sum(np.abs(data)**2, axis=-1)**(1./2) + 1e-12
+                tile_data_norm = np.tile(data_norm, (data.shape[1], 1))
+                data = data / tile_data_norm.T
+                fd_feature = file(os.path.join(self.bow_path, f), "wb")
+                fea = vl_coding(data, codebook, 3, 5, image_width, image_height, xs, ys)
+                np.save(fd_feature, fea)
 
 if __name__ == "__main__":
     word = 256
