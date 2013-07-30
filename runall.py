44cac07cc4670ccd5aca6c1c9fed3bcd259226d4
ShengyinWu <icylord@gmail.com>
2013-07-30 18:37:49 +0800

    code refactor

--- runall.py
+++ runall.py
@@ -4,15 +4,15 @@ import mybow as mb
 import trainandeval as te
 
 def main():
-	#1 extract feature
-    ef.main()
-    #2 do kmeans
-    mk.main()
-    #3 extract baf of words
-    mb.main()
+    # #1 extract feature
+    # ef.main()
+    # #2 do kmeans
+    # mk.main()
+    # #3 extract baf of words
+    # mb.main()
     #4 train and evaluation
     te.main()
 
 if __name__ == '__main__':
-	main()
+    main()
 
